befc174ced0f509298b9bc21f8553f549c05fa5c
Ja-baka <73071447+Ja-baka@users.noreply.github.com>
2022-02-23 14:36:25 +0300

    все .jpg были заменены на .png

---
+++ LogisticEBook/Apps/Topic2_4Photo_4.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LogisticEBook.Apps
+{
+	/// <summary>
+	/// Логика взаимодействия для Topic2_4Photo_27b.xaml
+	/// </summary>
+	public partial class Topic2_4Photo_4 : Window
+	{
+		public Topic2_4Photo_4()
+		{
+			InitializeComponent();
+		}
+	}
+}
